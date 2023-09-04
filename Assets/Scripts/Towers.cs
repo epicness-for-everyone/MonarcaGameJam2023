1a43ef97f51c4af55d54117cc72fe04d739ee2c7
Krows-MJ <lukakrows@gmail.com>
2023-09-04 15:52:05 -0400

    Actualizando spawn de unidades

--- Assets/Scripts/Towers.cs
+++ Assets/Scripts/Towers.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Towers : MonoBehaviour
 {
+    private Units ctrUnits;
     [SerializeField][Range(1,7)] private float spawnTimer;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private GameObject Unit;
     [SerializeField] private List<GameObject> listUnits;
 
-    private int sizeList= 10;
+    private int sizeList= 5;
     private float countTimer;
     private int o;
+    private GameObject ObjUnit;
     private void Awake(){
 
     }
@@ -19,7 +21,7 @@ public class Towers : MonoBehaviour
     void Start()
     {
         countTimer= 0;
-        //AddUnitsPool(sizeList);
+        AddUnitsPool(sizeList);
     }
 
     // Update is called once per frame
@@ -32,19 +34,22 @@ public class Towers : MonoBehaviour
         }
     }
 
-    public void RequestUnit(){
+    public GameObject RequestUnit(){
         for(o=0; o<listUnits.Count; o++){
             if(!listUnits[o].activeSelf){
-                //listUnits[o].SetActive(true);
-                //Objeto seleccionado
+                return listUnits[o];
             }
         }
         AddUnitsPool(1);
-        //listUnits[listUnits.Count - 1].SetActive(true);
-        //Objeto sleecionado
+        return listUnits[listUnits.Count -1];
     }
     public void SpawnUnit(){
-        Instantiate(Unit, spawnPoint.position, Quaternion.identity);
+        //Instantiate(Unit, spawnPoint.position, Quaternion.identity);
+        ObjUnit= RequestUnit();
+        ObjUnit.SetActive(true);
+        ObjUnit.transform.position= spawnPoint.position;
+        ctrUnits= ObjUnit.GetComponent<Units>();
+        ctrUnits.setMove(true);
     }
     private void AddUnitsPool(int n){
         //Crea n cantidad de objetos desactivandolos y añadiendolos a la lista
--- Assets/Scripts/Units.cs
+++ Assets/Scripts/Units.cs
@@ -6,24 +6,29 @@ public class Units : MonoBehaviour
 {
     [SerializeField] private int life;
     [SerializeField] private int damage;
+    [SerializeField][Range(0,3)] private float attackCooldown;
     [SerializeField] private float movementSpeed;
-    [SerializeField] private Vector2 direction;
+    [SerializeField][Range(-1,1)] private int direction;
 
-    private bool move;
+    [SerializeField]private bool move= false;
     private Vector3 dmov;
+    private void Awake(){}
     // Start is called before the first frame update
     void Start()
     {
-        move=true;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(move){
-            dmov= new Vector3(direction.x + movementSpeed, direction.y, 0);
-            transform.Translate(direction * Time.deltaTime);
+            dmov= new Vector3(direction * movementSpeed, 0, 0);
+            transform.Translate(dmov * Time.deltaTime);
         }
+    }
+
+    public void TakeDamage(){
+
     }
 
     //getters and setters
