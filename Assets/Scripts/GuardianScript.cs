7f18802b3caced67f4fb710ba57079cc2a8bfa6c
Raxorg <lrdolucho@gmail.com>
2023-09-09 14:17:54 -0400

    First commit

---
+++ Assets/Scripts/GuardianScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianScript : MonoBehaviour {
+
+    [SerializeField] private Transform guardianPosition;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
