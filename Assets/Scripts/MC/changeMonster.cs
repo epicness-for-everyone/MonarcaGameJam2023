81ce22ecf1574eabac74c363a39ba5c28d8e71fa
Noelia Martínez <noeowo384@gmail.com>
2023-09-10 11:05:51 -0400

    script condición para combinar piezas y cuerpo de monstro

--- Assets/Scripts/MC/changeMonster.cs
+++ Assets/Scripts/MC/changeMonster.cs
@@ -12,12 +12,12 @@ public class changeMonster : MonoBehaviour
     public GameObject MCCM;
     private Image MCPM1_IMG, MCPM2_IMG, MCCM_IMG;
     private Button MCPM1_BTN, MCPM2_BTN;
-    public Sprite[] PIECE_IMAGES;
-    public Sprite[] BODY_IMAGES;
+    public Sprite[] PIECE_MONSTER;
+    public Sprite[] BODY_MONSTER;
     private Sprite RANDOM;
     private Sprite RANDOM2;
-    public string PIECE_SELECT;
-    public string BODY_SELECT;
+    public string pieceSelect;
+    public string bodySelect;
     private void Awake(){
         MCPM1_IMG= MCPM1.GetComponent<Image>();
         MCPM2_IMG= MCPM2.GetComponent<Image>();
@@ -28,27 +28,26 @@ public class changeMonster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MCPM1_IMG.sprite = PIECE_IMAGES[Random.Range(0, PIECE_IMAGES.Length)];
-        MCPM2_IMG.sprite = PIECE_IMAGES[Random.Range(0, PIECE_IMAGES.Length)];
-        MCCM_IMG.sprite = BODY_IMAGES[Random.Range(0, BODY_IMAGES.Length)];
+        MCPM1_IMG.sprite = PIECE_MONSTER[Random.Range(0, PIECE_MONSTER.Length)];
+        MCPM2_IMG.sprite = PIECE_MONSTER[Random.Range(0, PIECE_MONSTER.Length)];
+        MCCM_IMG.sprite = BODY_MONSTER[Random.Range(0, BODY_MONSTER.Length)];
     }
 
     public void changeButton(string select){
-        if(select == "MCPM1") PIECE_SELECT = MCPM1_IMG.sprite.name;
-        else PIECE_SELECT = MCPM2_IMG.sprite.name;
-        BODY_SELECT =  MCCM_IMG.sprite.name;
-        Debug.Log("Selec "+ select + PIECE_SELECT + BODY_SELECT);
+        if(select == "MCPM1") pieceSelect = MCPM1_IMG.sprite.name;
+        else pieceSelect = MCPM2_IMG.sprite.name;
+        bodySelect =  MCCM_IMG.sprite.name;
         
-        RANDOM = PIECE_IMAGES[Random.Range(0, PIECE_IMAGES.Length)];
-        RANDOM2 = PIECE_IMAGES[Random.Range(0, PIECE_IMAGES.Length)];
+        RANDOM = PIECE_MONSTER[Random.Range(0, PIECE_MONSTER.Length)];
+        RANDOM2 = PIECE_MONSTER[Random.Range(0, PIECE_MONSTER.Length)];
         MCPM1_IMG.sprite = RANDOM;
         MCPM2_IMG.sprite = RANDOM2;
-        MCCM_IMG.sprite = BODY_IMAGES[Random.Range(0, BODY_IMAGES.Length)];
+        MCCM_IMG.sprite = BODY_MONSTER[Random.Range(0, BODY_MONSTER.Length)];
         MCPM1_BTN.enabled=false;
         MCPM2_BTN.enabled=false;
+        //incluir función para combinar -merge-
         Tareas.Nueva(3, disableButton);
-    }
-    
+    }  
 
     public void disableButton(){
         MCPM1_BTN.enabled=true;
---
+++ Assets/Scripts/MC/mergeMonster.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combineMonster : MonoBehaviour
+{
+    public string PIECE_SELECT;
+    public string BODY_SELECT;
+    public string gotMonster;
+    // Start is called before the first frame update cambiar a mergeMonster
+    void Start()
+    {
+
+    }
+
+    public void mergeResult(){
+        changeMonster piece = GetComponent<changeMonster>();
+        PIECE_SELECT = piece.pieceSelect;
+
+        changeMonster body = GetComponent<changeMonster>();
+        BODY_SELECT = body.bodySelect;
+
+        Debug.Log("merge "+PIECE_SELECT + BODY_SELECT);
+
+        switch(PIECE_SELECT)
+        {
+            case "PIECE_0":
+                switch(BODY_SELECT)
+                {
+                    case "BODY_0":
+                        Debug.Log("PIECE_0  BODY_0"+" cycplop04_walk");
+                    break;
+                    default:
+                        Debug.Log("PIECE_0  BODY_5");
+                    break;
+                }
+            break;
+            case "PIECE_1":
+                switch(BODY_SELECT)
+                {
+                    case "BODY_0":
+                        Debug.Log("PIECE_1  BODY_0"+" cycplop02_walk");
+                    break;
+                    default:
+                        Debug.Log("PIECE_1  BODY_0");
+                    break;
+                }
+            break;
+            case "PIECE_2":
+                switch(BODY_SELECT)
+                {
+                    case "BODY_0":
+                        Debug.Log("PIECE_2  BODY_0"+" cycplop01_walk");
+                    break;
+                    default:
+                        Debug.Log("PIECE_2  BODY_0");
+                    break;
+                }
+            break;
+            case "PIECE_3":
+                switch(BODY_SELECT)
+                {
+                    case "BODY_0":
+                        Debug.Log("PIECE_3  BODY_0"+" cycplop03_walk");
+                    break;
+                    default:
+                        Debug.Log("PIECE_3  BODY_0");
+                    break;
+                }
+            break;
+            default:
+                switch(BODY_SELECT)
+                {
+                    case "BODY_0":
+                        Debug.Log("PIECE_0  BODY_0"+" cycplop00_walk");
+                    break;
+                    default:
+                        Debug.Log("PIECE_0  BODY_0");
+                    break;
+                }
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
